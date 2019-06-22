642c93f4e2adf7e2cb5d15fd848f60751cbaeaa3
Judy <codelinked@gmail.com>
2019-06-22 15:46:24 -0400

    accordion js background added

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// Randomly assigns background colors to the books on the bookshelf/accordion
+let books = document.querySelectorAll(".book");
+let colors = [ '#74BDCB', '#FFA384', '#D8A7B1', '#B6E2D3', '#887BB0','#6DA6F6','#03AB99','#FFD571','#AEC3BC','#A58CB3', '#80b34d', '#ff9933', '#6699cc', '#003366'];
+
+
+window.addEventListener("load", () => {
+  for (i = 0; i < books.length; i++) {
+    books[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    books[i].style.opacity = "0.8";
+  }
+});
