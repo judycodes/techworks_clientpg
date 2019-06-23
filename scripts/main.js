2798a004124d0691f42b0f55c551af6bb570f72c
Judy <codelinked@gmail.com>
2019-06-23 14:49:34 -0400

    background to accordion added

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,21 @@
-// Randomly assigns background colors to the books on the bookshelf/accordion
+/*Randomly assigns background colors and a shadow to the books on the bookshelf/accordion*/
+
+//selects all books on bookshelf
 let books = document.querySelectorAll(".book");
+
+//colors for book spines
 let colors = [ '#74BDCB', '#FFA384', '#D8A7B1', '#B6E2D3', '#887BB0','#6DA6F6','#03AB99','#FFD571','#AEC3BC','#A58CB3', '#80b34d', '#ff9933', '#6699cc', '#003366'];
 
 
 window.addEventListener("load", () => {
   for (i = 0; i < books.length; i++) {
     books[i].style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    books[i].style.opacity = "0.8";
-    books[i].style.boxShadow = "0px 2.5px rgba(0,0,0,0.3)";
+    books[i].style.opacity = "0.85";
+    books[i].style.boxShadow = "0px 2.5px rgba(0,0,0,0.35)";
   }
 });
 
-//Creates a book pull out animation
+/*Creates a subtle book pull out animation before revealing accordion slide/book cover*/
 for (let i = 0; i < books.length; i++) {
   books[i].addEventListener('mouseover', () => {
     books[i].classList.add('scale');
